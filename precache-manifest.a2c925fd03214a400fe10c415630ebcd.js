297b14b3318c35ff4ec00b7709a5bb9fea28ea9f
Phodal HUANG <h@phodal.com>
2019-09-23 17:19:55 +0800

    Updates

---
+++ precache-manifest.a2c925fd03214a400fe10c415630ebcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4704977c7da925dc52be8dccbb0fd2e7",
+    "url": "/xizhi/index.html"
+  },
+  {
+    "revision": "268cad6d3dc76d86b120",
+    "url": "/xizhi/static/css/2.e8d046a2.chunk.css"
+  },
+  {
+    "revision": "88761f16698657a27ed6",
+    "url": "/xizhi/static/css/main.9a5c1625.chunk.css"
+  },
+  {
+    "revision": "268cad6d3dc76d86b120",
+    "url": "/xizhi/static/js/2.d2786c67.chunk.js"
+  },
+  {
+    "revision": "88761f16698657a27ed6",
+    "url": "/xizhi/static/js/main.4959d999.chunk.js"
+  },
+  {
+    "revision": "fc472f3eda26615e7329",
+    "url": "/xizhi/static/js/runtime~main.a3290b4f.js"
+  }
+]);
