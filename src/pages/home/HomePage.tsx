a43565ea63e772f74427a7f7a21baa6fe2d3a240
Phodal HUANG <h@phodal.com>
2019-09-23 15:17:45 +0800

    feat: import draft-js

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,17 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component, Props} from 'react';
+import {Editor, EditorState} from 'draft-js';
 
-import PhdCard from "../../components/Card/Card";
-import PhdInput from "../../components/PhdInput/PhdInput";
-import Counter from '../../components/Counter/Counter';
+interface HomeState {
+  editorState: EditorState
+}
 
-class HomePage extends Component {
+class HomePage extends Component <any, HomeState>{
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty()
+    };
+  }
+
+  onChange = (editorState: EditorState) => {
+    return this.setState({editorState});
+  };
+
   render() {
     return (
       <div className="home">
-        <Counter></Counter>
-        home
-        <PhdInput></PhdInput>
-        <PhdCard></PhdCard>
+        <Editor editorState={this.state.editorState} onChange={this.onChange}/>
       </div>
     )
   };
